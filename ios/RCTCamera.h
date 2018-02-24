24d6208c7aa4d3ec0ad10eb516e9d231843d7601
chenjianwei <938548157@qq.com>
2018-02-24 15:00:16 +0800

    fix

--- ios/RCTCamera.h
+++ ios/RCTCamera.h
@@ -1,6 +1,17 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 #import "CameraFocusSquare.h"
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTBridge.h>
+#import <React/RCTLog.h>
+#import <React/RCTUtils.h>
+#import <React/RCTEventDispatcher.h>
+#else
+#import "RCTBridge.h"
+#import "RCTLog.h"
+#import "RCTUtils.h"
+#import "RCTEventDispatcher.h"
+#endif
 
 @class RCTCameraManager;
 
--- ios/RCTCamera.m
+++ ios/RCTCamera.m
@@ -1,19 +1,6 @@
 #import "RCTCamera.h"
 #import "RCTCameraManager.h"
-#if __has_include(<React/RCTBridge.h>)
-#import <React/RCTBridge.h>
-#import <React/RCTLog.h>
-#import <React/RCTUtils.h>
-#import <React/RCTEventDispatcher.h>
-#else
-#import "RCTBridge.h"
-#import "RCTLog.h"
-#import "RCTUtils.h"
-#import "RCTEventDispatcher.h"
-#endif
-
 #import "UIView+React.h"
-
 #import <AVFoundation/AVFoundation.h>
 #import "CameraFocusSquare.h"
 
